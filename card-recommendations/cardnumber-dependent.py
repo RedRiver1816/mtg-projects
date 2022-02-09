447bdafc82234fba708ed206d91f0106936476f2
May Flowers <jambobambo1019@gmail.com>
2022-02-08 23:49:12 -0500

    Add files via upload

---
+++ card-recommendations/cardnumber-dependent.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def TopCardRecs1(cardname, deckmatrix):
+    
+    import numpy as np
+    
+    relatedtups = []
+    
+    if cardname not in deckmatrix.columns:
+        return print('Card not available.')
+    
+    else:
+        
+        cardnamematrix = deckmatrix[deckmatrix[cardname] > 0].drop(cardname, axis = 1)
+        
+        for diffcards in cardnamematrix.columns:
+            relatedtups.append((diffcards, np.mean(cardnamematrix[diffcards])))
+    
+    return sorted(relatedtups, key = lambda x:x[1], reverse = True)[:10]
